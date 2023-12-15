938b78d52f0416a3b0101c314fca2327aa7c422c
james <james1.6@hotmail.com>
2023-12-15 22:25:27 +1030

    Correct Stage 5 finish

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,44 +1,40 @@
 package tictactoe;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
+
+    private static final int[][] boardArray = new int[3][3];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 //        Scanner scanner = new Scanner("2 2\r\n2 2\r\ntwo two\r\n1 4\r\n1 1\r\n3 3\r\n2 1\r\n3 1\r\n2 3\r\n3 2"); //testing data
-//        String input = scanner.next();
-        char player;
+        char player = 'X';
         int[] coords;
 
-        int[][] gridArray = new int[3][3];
-
-        for (int j = 0, k = 0; j < 3; j++) { //fill grid array from input
-            for (int i = 0; i < 3; i++) {
-                gridArray[j][i] = input.charAt(k); // == (int)'_'?' ':input.charAt(k);
-                k++;
-            }
+        for (int j = 0; j < 3; j++) { //initial fill of the board array with spaces
+            Arrays.fill(boardArray[j],' ');//board array is just the slots for gameplay
         }
+        System.out.println(getBoardString()); //draw the blank board
 
 
-        String grid = getGridString(gridArray);
-        System.out.println(grid);
-        System.out.println(getGameState(gridArray));
-//        parse input
-        coords = getCoords(scanner, gridArray);
-        player = 'X';
-
-        //update grid array
-        gridArray[coords[0]][coords[1]] = player;
-        grid = getGridString(gridArray);
-        System.out.println(grid); //print updated grid
+        while (getGameState().equals("Game not finished")) {
+            coords = getValidMove(scanner); //        get the next valid location to play a piece
+            boardArray[coords[0]][coords[1]] = player; //play the move into the boardArray
+            System.out.println(getBoardString()); //print updated board
+            player = player == 'X' ? 'O': 'X'; // toggle player to the other
+        }
+        System.out.println(getGameState()); //victory line
 
     }
 
-    private static int[] getCoords(Scanner scanner, int[][] gridArray) {
+    private static int[] getValidMove(Scanner scanner) {
         String input;
         while(true) {
             input = scanner.next() + scanner.next();
+            System.out.println(input);
 
             if (!Character.isDigit(input.charAt(0)) || !Character.isDigit(input.charAt(1))) {
                 System.out.println("You should enter numbers!");
@@ -50,23 +46,23 @@ public class Main {
                 System.out.println("Coordinates should be from 1 to 3!");
                 continue;
             }
-            if (gridArray[y][x] != (int) '_') {
+            if (boardArray[y][x] != (int) ' ') {
                 System.out.println("This cell is occupied! Choose another one!");
                 continue;
             }return new int[]{y, x};
         }
     }
 
-    private static String getGridString(int[][] gridArray) {
-        char a = (char) gridArray[0][0];
-        char b = (char) gridArray[0][1];
-        char c = (char) gridArray[0][2];
-        char d = (char) gridArray[1][0];
-        char e = (char) gridArray[1][1];
-        char f = (char) gridArray[1][2];
-        char g = (char) gridArray[2][0];
-        char h = (char) gridArray[2][1];
-        char i = (char) gridArray[2][2];
+    private static String getBoardString() {
+        char a = (char) boardArray[0][0];
+        char b = (char) boardArray[0][1];
+        char c = (char) boardArray[0][2];
+        char d = (char) boardArray[1][0];
+        char e = (char) boardArray[1][1];
+        char f = (char) boardArray[1][2];
+        char g = (char) boardArray[2][0];
+        char h = (char) boardArray[2][1];
+        char i = (char) boardArray[2][2];
 
         return """
                 ---------
@@ -76,8 +72,8 @@ public class Main {
                 ---------""".formatted(a,b,c,d,e,f,g,h,i);
     }
 
-    private static String getGameState(int[][] gridArray) {
-        int[][] lineArray = getLineArray(gridArray);
+    private static String getGameState() {
+        int[][] lineArray = getLineArray();
         boolean xTrip = false;
         boolean oTrip = false;
         int xCount = 0;
@@ -90,7 +86,7 @@ public class Main {
                 oTrip = true;
             }
         }
-        for (int[] line : gridArray) {
+        for (int[] line : boardArray) {
             for (int cell : line) {
                 if (cell == 88) {
                     xCount++;
@@ -101,39 +97,39 @@ public class Main {
             }
 
         }
-        String verdict;
+        String gameState;
 
         if ((xTrip && oTrip) || Math.abs(xCount - oCount) > 1) {
-            verdict = "Impossible";
+            gameState = "Impossible";
         } else if (!xTrip && !oTrip && (xCount + oCount) == 9) {
-            verdict = "Draw";
+            gameState = "Draw";
         } else if (!xTrip && !oTrip && (xCount + oCount) != 9) {
-            verdict = "Game not finished";
+            gameState = "Game not finished";
         } else if (xTrip) {
-            verdict = "X wins";
+            gameState = "X wins";
         } else if (oTrip) {
-            verdict = "O wins";
+            gameState = "O wins";
         } else {
-            verdict = "Undefined game state.";
+            gameState = "Undefined game state.";
         }
 
-        return verdict;
+        return gameState;
 
     }
 
-    private static int[][] getLineArray(int[][] gridArray) {
+    private static int[][] getLineArray() {
         int[][] lineArray = new int [8][3];
-        System.arraycopy(gridArray, 0, lineArray, 0, 3); //horizontal lines
+        System.arraycopy(boardArray, 0, lineArray, 0, 3); //horizontal lines
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                lineArray[j+3][i] = gridArray[i][j]; //vertical lines
+                lineArray[j+3][i] = boardArray[i][j]; //vertical lines
             }
         }
         for (int i = 0; i < 3; i++) {
-            lineArray[6][i] = gridArray[i][i]; //diagonal top left to bottom right
+            lineArray[6][i] = boardArray[i][i]; //diagonal top left to bottom right
         }
         for (int i = 0; i < 3; i++) {
-            lineArray[7][i] = gridArray[i][2 - i];//diagonal top right to bottom left
+            lineArray[7][i] = boardArray[i][2 - i];//diagonal top right to bottom left
         }
         return lineArray;
     }
